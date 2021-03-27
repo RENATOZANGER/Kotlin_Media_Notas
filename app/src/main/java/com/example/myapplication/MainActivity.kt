06e82fb2fa637985a3ac6ba53053b6bd3caf6b6c
RenatoZangerolami <renato.zanger@gmail.com>
2021-03-27 12:19:52 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.myapplication
+
+import android.graphics.Color
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val btCalcular = calcular
+        val resultado = resultado
+        btCalcular.setOnClickListener {
+            val nota1 = Integer.parseInt(nota1.text.toString())
+            val nota2 = Integer.parseInt(nota2.text.toString())
+            val media = (nota1 + nota2)/2
+            val faltas = Integer.parseInt(faltas.text.toString())
+
+            if(media >=6 && faltas <=10){
+                resultado.setText("Aluno foi Aprovado" + "\n" + "Nota Final:" + media + "\n" +"Faltas:" + faltas)
+                resultado.setTextColor(Color.GREEN)
+            }else{
+                resultado.setText("Aluno foi Reprovado" + "\n" + "Nota Final:" + media + "\n" +"Faltas:" + faltas)
+                resultado.setTextColor(Color.RED)
+            }
+        }
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
